b730d337f815f9baccfdde35163076117ce22bf2
Kevin Ketterer <kevin-ketterer@users.noreply.github.com>
2019-02-03 12:57:00 -0600

    Finished Ravenous

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -9,7 +9,7 @@ class BusinessList extends React.Component {
       <div className="BusinessList">
         {
           this.props.businesses.map(business => {
-            return <Business business={business} />
+            return <Business business={this.state.business} key={business.id} />
           })
         }
       </div>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends React.Component {
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
-    
+    this.handleSortByChange = this.handleSortByChange.bind(this);
 
     this.sortByOptions = {
       'Best Match': 'best_match',
@@ -53,7 +53,7 @@ class SearchBar extends React.Component {
       let sortByOptionValue = this.sortByOptions[sortByOption];
       return (<li className={this.getSortByClass(sortByOptionValue)}
                   key={sortByOptionValue}
-                  onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+                  onClick={this.handleSortByChange}>
                 {sortByOption}
              </li>);
     });
